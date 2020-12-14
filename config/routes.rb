ddbd862bf3e7e24f177d37ed9a2383713e03ce6c
Roberto Basulto <roberto.basulto085@gmail.com>
2020-12-13 19:42:23 -0500

    Created namespaces for update and destroy for items and merchants

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: [:index, :show, :create]
-      resources :items, only: [:index, :show, :create]
+      resources :merchants, only: [:index, :show, :create, :update, :destroy]
+      resources :items, only: [:index, :show, :create, :update, :destroy]
     end
   end
 end
