72fbd18be77a2ea8e3919a12c5ca029ae2e2be70
Roberto Basulto <roberto.basulto085@gmail.com>
2020-12-14 13:59:46 -0500

    Implemented items route and updated serializers

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -6,21 +6,26 @@ class Api::V1::MerchantsController < ApplicationController
 
   def show
     merchant = Merchant.find(params[:id])
-    render json: SingleMerchantSerializer.format_merchant(merchant)
+    render json: MerchantSerializer.format_merchant(merchant)
   end
 
   def create
     merchant = Merchant.create(merchant_params)
-    render json: SingleMerchantSerializer.format_merchant(merchant)
+    render json: MerchantSerializer.format_merchant(merchant)
   end
 
   def update
     merchant = Merchant.update(params[:id], merchant_params)
-    render json: SingleMerchantSerializer.format_merchant(merchant)
+    render json: MerchantSerializer.format_merchant(merchant)
   end
 
   def destroy
     render json: Merchant.delete(params[:id])
+  end
+
+  def items
+    items = Merchant.fetch_items(params[:merchant_id])
+    render json: ItemSerializer.format_items(items)
   end
 
   private
