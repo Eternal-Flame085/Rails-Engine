08748e497695537ba3c0570467445f25e906303c
Roberto Basulto <roberto.basulto085@gmail.com>
2020-12-15 13:17:00 -0500

    Rafactored from the search controllers

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,20 @@ class Item < ApplicationRecord
   def self.fetch_merchant(item_id)
     self.find(item_id).merchant
   end
+
+  def self.find_item(attribute, value)
+    where("#{attribute} ILIKE ?", "%#{value}%").first
+  end
+
+  def self.find_item_with_unit_price(value)
+    where(unit_price: value).first
+  end
+
+  def self.find_items_with_unit_price(value)
+    where(unit_price: value)
+  end
+
+  def self.find_all_items_search(attribute, value)
+    where("#{attribute} ILIKE ?", "%#{value}%")
+  end
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -6,4 +6,12 @@ class Merchant < ApplicationRecord
   def self.fetch_items(id)
     Item.where(merchant_id: id)
   end
+
+  def self.find_all_merchant_search(attribute, value)
+    where("#{attribute} ILIKE ?", "%#{value}%")
+  end
+
+  def self.find_merchant(attribute, value)
+    where("#{attribute} ILIKE ?", "%#{value}%").first
+  end
 end
