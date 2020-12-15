b928527ec160493d06c7f77f386a05b86cf713eb
Roberto Basulto <roberto.basulto085@gmail.com>
2020-12-15 13:16:41 -0500

    Refacotred from the search controllers

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -20,5 +20,58 @@ RSpec.describe Item, type: :model do
 
       expect(Item.fetch_merchant(item.id)).to eq(item_merchant)
     end
+
+    it ".find_item returns an Item based on the attribute and value given" do
+      item = create(:item, name: 'CyberPunk2077')
+      attribute = 'name'
+      value = 'punk'
+
+      item = Item.find_item(attribute, value)
+
+      expect(item).to be_a Item
+      expect(item.name).to eq('CyberPunk2077')
+    end
+
+    it ".find_all_items_seach returns an array of Item objects that match the value given" do
+      create(:item, name: 'CyberPunk2077')
+      create(:item, name: 'CloudPunk')
+      create(:item, name: 'Just different')
+      attribute = 'name'
+      value = 'punk'
+
+      items = Item.find_all_items_search(attribute, value)
+
+      expect(items[0]).to be_a Item
+      expect(items[0].name).to eq('CyberPunk2077')
+
+      expect(items[1]).to be_a Item
+      expect(items[1].name).to eq('CloudPunk')
+    end
+
+    it ".find_item will return one item with matching unit price" do
+      create(:item, unit_price: 60.0)
+      create(:item, unit_price: 60.0)
+      value = 60.0
+
+      item = Item.find_item_with_unit_price(value)
+
+      expect(item).to be_a Item
+      expect(item.unit_price).to eq(value)
+    end
+
+    it ".find_items_with_unit_price returns an array of Items that have a matching unit price" do
+      create(:item, unit_price: 60.0)
+      create(:item, unit_price: 60.0)
+      create(:item, unit_price: 50.0)
+      value = 60.0
+
+      items = Item.find_items_with_unit_price(value)
+
+      expect(items[0]).to be_a Item
+      expect(items[0].unit_price).to eq(value)
+
+      expect(items[1]).to be_a Item
+      expect(items[1].unit_price).to eq(value)
+    end
   end
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -19,5 +19,32 @@ RSpec.describe Merchant, type: :model do
       expect(Merchant.fetch_items(merchant.id)[1]).to eq(items[1])
       expect(Merchant.fetch_items(merchant.id)[2]).to eq(items[2])
     end
+
+    it ".find_merchant returns a merchant based on the attribute and value given" do
+      merchant = create(:merchant, name: 'CDProject Red')
+      attribute = 'name'
+      value = 'red'
+
+      merchant = Merchant.find_merchant(attribute, value)
+
+      expect(merchant).to be_a Merchant
+      expect(merchant.name).to eq('CDProject Red')
+    end
+
+    it ".find_all_merchant_seach returns an array of Merchant objects that match the value given" do
+      create(:merchant, name: 'CDProject Red')
+      create(:merchant, name: 'Red Camera Company')
+      create(:merchant, name: 'Just different')
+      attribute = 'name'
+      value = 'red'
+
+      merchants = Merchant.find_all_merchant_search(attribute, value)
+
+      expect(merchants[0]).to be_a Merchant
+      expect(merchants[0].name).to eq('CDProject Red')
+
+      expect(merchants[1]).to be_a Merchant
+      expect(merchants[1].name).to eq('Red Camera Company')
+    end
   end
 end
