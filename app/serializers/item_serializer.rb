c071ef366dc90323076e850ee8ea9f63abd59cb1
Roberto Basulto <roberto.basulto085@gmail.com>
2020-12-14 22:03:18 -0500

    Updated serializers with fastapi

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,34 +1,4 @@
 class ItemSerializer
-  def self.format_items(items)
-      {
-        data:
-          items.map do |item|
-          {
-            id: item.id,
-              type: "Item",
-              attributes: {
-                name: item.name,
-                description: item.description,
-                unit_price: item.unit_price,
-                merchant_id: item.merchant_id,
-              }
-          }
-        end
-      }
-  end
-
-  def self.format_item(item)
-    {
-      data: {
-        id: item.id,
-        type: "item",
-        attributes: {
-          name: item.name,
-          description: item.description,
-          unit_price: item.unit_price,
-          merchant_id: item.merchant_id,
-        }
-      }
-    }
-  end
+  include FastJsonapi::ObjectSerializer
+  attributes :name, :description, :unit_price, :merchant_id
 end
--- app/serializers/merchant_serializer.rb
+++ app/serializers/merchant_serializer.rb
@@ -1,28 +1,4 @@
 class MerchantSerializer
-  def self.format_merchants(merchants)
-      {
-        data:
-          merchants.map do |merchant|
-          {
-            id: merchant.id,
-              type: "merchant",
-              attributes: {
-                name: merchant.name,
-              }
-          }
-        end
-      }
-  end
-
-  def self.format_merchant(merchant)
-    {
-      data: {
-        id: merchant.id,
-        type: "Merchant",
-        attributes: {
-          name: merchant.name
-        }
-      }
-    }
-  end
+  include FastJsonapi::ObjectSerializer
+  attributes :name
 end
