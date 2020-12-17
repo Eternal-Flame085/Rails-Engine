3a8407de399673f43d1ae7db3cec3f51a698186c
Roberto Basulto <roberto.basulto085@gmail.com>
2020-12-17 00:50:44 -0500

    Updated methods to pass in the harness

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -16,12 +16,12 @@ class Api::V1::ItemsController < ApplicationController
   end
 
   def destroy
-    render json: Item.delete(params[:id])
+    Item.delete(params[:id])
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+    params.permit(:name, :description, :unit_price, :merchant_id)
   end
 end
