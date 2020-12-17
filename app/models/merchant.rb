ceeec4720ca70f48cdda213b07cce21ad022a447
Roberto Basulto <roberto.basulto085@gmail.com>
2020-12-17 13:07:09 -0500

    Added model method to get the merchant revenue

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -27,13 +27,21 @@ class Merchant < ApplicationRecord
   def self.find_most_items_sold(quantity)
     select("merchants.*, SUM(invoice_items.quantity) AS items_sold")
     .joins(invoices: [:invoice_items, :transactions])
-    .where(transactions: {result: "success"}).group(:id)
+    .where(transactions: {result: "success"})
+    .group(:id)
     .order("items_sold DESC")
     .limit(quantity)
   end
 
   def self.total_revenue_across_dates(start_date, end_date)
     joins(invoices: [:invoice_items, :transactions])
-    .where(transactions: {result: 'success'}, invoices: {created_at: (start_date)..("#{end_date} 23:59:59"), status: "shipped"}).sum("invoice_items.quantity * invoice_items.unit_price")
+    .where(transactions: {result: 'success'}, invoices: {created_at: (start_date)..("#{end_date} 23:59:59"), status: "shipped"})
+    .sum("invoice_items.quantity * invoice_items.unit_price")
+  end
+
+  def self.total_revenue_for_merchant(merchant_id)
+    joins(invoices: [:invoice_items, :transactions])
+    .where(transactions: {result: 'success'}, merchants: {id: merchant_id}, invoices: {status: "shipped"})
+    .sum("invoice_items.quantity * invoice_items.unit_price")
   end
 end
