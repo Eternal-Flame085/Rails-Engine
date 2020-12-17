095b6f391492f98aee7afedb4ffc0341ba431d31
Roberto Basulto <roberto.basulto085@gmail.com>
2020-12-16 19:40:02 -0500

    Added most revenue and most items routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
         get '/:id/items', to: 'merchants#index'
         get '/find', to: 'search#find_merchant'
         get '/find_all', to: 'search#find_all'
+        get '/most_revenue', to: 'search#most_revenue'
+        get '/most_items', to: 'search#most_items'
       end
 
       namespace :items do
