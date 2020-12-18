2560a87f955444b1a9febce2d439949b13fa9e7b
Roberto Basulto <roberto.basulto085@gmail.com>
2020-12-17 22:47:04 -0500

    refactored search facade

--- app/facade/search_facade.rb
+++ app/facade/search_facade.rb
@@ -1,20 +1,12 @@
 class SearchFacade
   def self.find_item(params)
     attribute = params.keys.first
-    if attribute == 'unit_price'
-      Item.find_item_with_unit_price(params[attribute])
-    else
-      Item.find_item(attribute, params[attribute])
-    end
+    Item.find_item(attribute, params[attribute])
   end
 
   def self.find_all_items(params)
     attribute = params.keys.first
-    if attribute == 'unit_price'
-      Item.find_items_with_unit_price(params[attribute])
-    else
-      Item.find_all_items_search(attribute, params[attribute])
-    end
+    Item.find_all_items_search(attribute, params[attribute])
   end
 
   def self.find_merchant(params)
