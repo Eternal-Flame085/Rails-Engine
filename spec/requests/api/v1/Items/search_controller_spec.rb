d8dbf6beb1125d1873f2f9c3d8d8e2cbec0ce1dd
Roberto Basulto <roberto.basulto085@gmail.com>
2020-12-18 10:21:03 -0500

    Added missing edge cases

--- spec/requests/api/v1/Items/search_controller_spec.rb
+++ spec/requests/api/v1/Items/search_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'Finders' do
-  it "find returns a single Item record" do
+  it "find returns a single Item record using a name" do
     item = create(:item)
     attribute = 'name'
 
@@ -25,6 +25,60 @@ describe 'Finders' do
 
     expect(item_response[:data][:attributes]).to have_key(:name)
     expect(item_response[:data][:attributes][:name]).to be_a(String)
+  end
+
+  it "find returns a single Item record using an id" do
+    item = create(:item)
+    attribute = 'id'
+
+    get "/api/v1/items/find?#{attribute}=#{item.id}"
+
+    expect(response).to be_successful
+
+    item_response = JSON.parse(response.body, symbolize_names: true)
+
+    expect(item_response).to have_key(:data)
+    expect(item_response[:data]).to be_an(Hash)
+
+    expect(item_response[:data]).to have_key(:id)
+    expect(item_response[:data][:id]).to be_an(String)
+
+    expect(item_response[:data]).to have_key(:type)
+    expect(item_response[:data][:type]).to be_a(String)
+
+    expect(item_response[:data]).to have_key(:attributes)
+    expect(item_response[:data][:attributes]).to be_a(Hash)
+
+    expect(item_response[:data][:attributes]).to have_key(:name)
+    expect(item_response[:data][:attributes][:name]).to be_a(String)
+  end
+
+  it "find returns a single Item record using created_at" do
+    item = create(:item, created_at: '2012-03-27 00:00:00')
+    attribute = 'created_at'
+    value = '2012-03-27'
+
+    get "/api/v1/items/find?#{attribute}=#{value}"
+
+    expect(response).to be_successful
+
+    item_response = JSON.parse(response.body, symbolize_names: true)
+
+    expect(item_response).to have_key(:data)
+    expect(item_response[:data]).to be_an(Hash)
+
+    expect(item_response[:data]).to have_key(:id)
+    expect(item_response[:data][:id]).to be_an(String)
+
+    expect(item_response[:data]).to have_key(:type)
+    expect(item_response[:data][:type]).to be_a(String)
+
+    expect(item_response[:data]).to have_key(:attributes)
+    expect(item_response[:data][:attributes]).to be_a(Hash)
+
+    expect(item_response[:data][:attributes]).to have_key(:name)
+    expect(item_response[:data][:attributes][:name]).to be_a(String)
+    expect(item_response[:data][:attributes][:name]).to eq(item.name)
   end
 
   it "find_all returns all matching item records using a name attribute" do
@@ -65,6 +119,38 @@ describe 'Finders' do
     create(:item, description: 'Just different')
     attribute = 'description'
     value = 'punk'
+
+    get "/api/v1/items/find_all?#{attribute}=#{value}"
+
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body, symbolize_names: true)
+
+    expect(items).to have_key(:data)
+    expect(items[:data]).to be_an(Array)
+    expect(items[:data].count).to eq(2)
+
+    items[:data].each do |item|
+      expect(item).to have_key(:id)
+      expect(item[:id]).to be_an(String)
+
+      expect(item).to have_key(:type)
+      expect(item[:type]).to be_a(String)
+
+      expect(item).to have_key(:attributes)
+      expect(item[:attributes]).to be_a(Hash)
+
+      expect(item[:attributes]).to have_key(:name)
+      expect(item[:attributes][:name]).to be_a(String)
+    end
+  end
+
+  it "find_all returns all matching item records using a name attribute" do
+    create(:item, name: 'CyberPunk2077', created_at: '2012-03-27 00:00:00')
+    create(:item, name: 'CloudPunk', created_at: '2012-03-27 23:59:59')
+    create(:item, name: 'Just different')
+    attribute = 'created_at'
+    value = '2012-03-27'
 
     get "/api/v1/items/find_all?#{attribute}=#{value}"
 
--- spec/requests/api/v1/merchants/search_controller_spec.rb
+++ spec/requests/api/v1/merchants/search_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'Finders' do
-  it "find returns a single merchant record" do
+  it "find returns a single merchant record by name" do
     merchant = create(:merchant)
     attribute = 'name'
 
@@ -27,12 +27,98 @@ describe 'Finders' do
     expect(merchant_call[:data][:attributes][:name]).to be_a(String)
   end
 
+  it "find returns a single merchant record by id" do
+    merchant = create(:merchant)
+    attribute = 'id'
+
+    get "/api/v1/merchants/find?#{attribute}=#{merchant.id}"
+
+    expect(response).to be_successful
+
+    merchant_call = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchant_call).to have_key(:data)
+    expect(merchant_call[:data]).to be_an(Hash)
+
+    expect(merchant_call[:data]).to have_key(:id)
+    expect(merchant_call[:data][:id]).to be_an(String)
+
+    expect(merchant_call[:data]).to have_key(:type)
+    expect(merchant_call[:data][:type]).to be_a(String)
+
+    expect(merchant_call[:data]).to have_key(:attributes)
+    expect(merchant_call[:data][:attributes]).to be_a(Hash)
+
+    expect(merchant_call[:data][:attributes]).to have_key(:name)
+    expect(merchant_call[:data][:attributes][:name]).to be_a(String)
+  end
+
+  it "find returns a single merchant record by created_at" do
+    merchant = create(:merchant, created_at: '2012-03-27 00:00:00')
+    attribute = 'created_at'
+    value = '2012-03-27'
+
+    get "/api/v1/merchants/find?#{attribute}=#{value}"
+
+    expect(response).to be_successful
+
+    merchant_call = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchant_call).to have_key(:data)
+    expect(merchant_call[:data]).to be_an(Hash)
+
+    expect(merchant_call[:data]).to have_key(:id)
+    expect(merchant_call[:data][:id]).to be_an(String)
+
+    expect(merchant_call[:data]).to have_key(:type)
+    expect(merchant_call[:data][:type]).to be_a(String)
+
+    expect(merchant_call[:data]).to have_key(:attributes)
+    expect(merchant_call[:data][:attributes]).to be_a(Hash)
+
+    expect(merchant_call[:data][:attributes]).to have_key(:name)
+    expect(merchant_call[:data][:attributes][:name]).to be_a(String)
+    expect(merchant_call[:data][:attributes][:name]).to eq(merchant.name)
+  end
+
   it "find_all returns all matching merchant records using a name attribute" do
     create(:merchant, name: 'CDProject Red')
     create(:merchant, name: 'Red Camera Company')
     create(:merchant, name: 'Just different')
     attribute = 'name'
     value = 'red'
+
+    get "/api/v1/merchants/find_all?#{attribute}=#{value}"
+
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchants).to have_key(:data)
+    expect(merchants[:data]).to be_an(Array)
+    expect(merchants[:data].count).to eq(2)
+
+    merchants[:data].each do |merchant|
+      expect(merchant).to have_key(:id)
+      expect(merchant[:id]).to be_an(String)
+
+      expect(merchant).to have_key(:type)
+      expect(merchant[:type]).to be_a(String)
+
+      expect(merchant).to have_key(:attributes)
+      expect(merchant[:attributes]).to be_a(Hash)
+
+      expect(merchant[:attributes]).to have_key(:name)
+      expect(merchant[:attributes][:name]).to be_a(String)
+    end
+  end
+
+  it "find_all returns all matching merchant records using a created_at attribute" do
+    create(:merchant, name: 'CDProject Red', created_at: '2012-03-27 00:00:00')
+    create(:merchant, name: 'Red Camera Company', created_at: '2012-03-27 23:59:59')
+    create(:merchant, name: 'Just different')
+    attribute = 'created_at'
+    value = '2012-03-27'
 
     get "/api/v1/merchants/find_all?#{attribute}=#{value}"
 
